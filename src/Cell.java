314f7fa3087cd2bbb3132c2d1060ccf3b0831b62
Courtney Wehner <126522587+cwehner19@users.noreply.github.com>
2023-04-27 21:13:14 -0500

    Update Cell.java

--- src/Cell.java
+++ src/Cell.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Cell {
+public class Cell implements Update {
+    private boolean isAlive;
+
+    public Cell(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+
+    @Override
+    public void Update() {
+
+    }
+
+    @Override
+    public void Update(int numAliveNeighbors) {
+        if (isAlive) {
+            // An alive cell with 2 or 3 alive neighbors stays alive; otherwise, it dies.
+            isAlive = numAliveNeighbors == 2 || numAliveNeighbors == 3;
+        } else {
+            // A dead cell with exactly 3 alive neighbors comes to life.
+            isAlive = numAliveNeighbors == 3;
+        }
+    }
 }
