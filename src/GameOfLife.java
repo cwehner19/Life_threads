0b8c7a6ad7b575e3015b0eb4b0c236a0dd07cdba
Courtney Wehner <126522587+cwehner19@users.noreply.github.com>
2023-04-27 21:14:11 -0500

    Update GameOfLife.java

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class GameOfLife {
+public class GameOfLife {
+    public static void main(String[] args) {
+        String inputFile = "initial_state.txt";
+        int numRows = 20;
+        int numColumns = 20;
+        int numGenerations = 15;
+        System.out.println("Welcome to Conway's Game of Life!");
+
+        Board board = new Board(numRows, numColumns);
+        board.readFromFile(inputFile);
+
+        System.out.println("Initial configuration: ");
+        board.printBoard();
+
+        Game game = new Game(board, numGenerations);
+        game.startGame();
+    }
 }
